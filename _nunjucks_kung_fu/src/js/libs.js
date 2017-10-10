284a2e4876cd00bcf03c55f2797e4ed3192d1eb3
Anton Ustinov <antonustinoff@mail.ru>
2017-10-11 00:15:38 +0400

    header & foooter

--- _nunjucks_kung_fu/src/js/libs.js
+++ _nunjucks_kung_fu/src/js/libs.js
@@ -5,3 +5,4 @@
 //=include ../libs/custom-scrollbar/jquery.mCustomScrollbar.concat.min.js
 //=include ../libs/waypoints.min.js
 //=include ../libs/jquery.equalheights.min.js
+//=include ../../bower_components/jquery.maskedinput/src/jquery.maskedinput.js
--- _nunjucks_kung_fu/src/js/main.js
+++ _nunjucks_kung_fu/src/js/main.js
@@ -11,16 +11,37 @@ $(document).ready(function () {
 	});
 
 	//Custom Input
-	$('.js-su-input').focus(function (){
-		$(this).parent('.su-input__box').addClass('is-focus');
+	$('.js-sa-input').focus(function (){
+		$(this).parent('.sa-input__box').addClass('is-focus');
 	});
 
-	$('.js-su-input').blur(function(){
+	$('.js-sa-input').blur(function(){
 		if($(this).val() == ''){
-			$(this).parent('.su-input__box').removeClass('is-focus')
+			$(this).parent('.sa-input__box').removeClass('is-focus');
 		}
 
 	});
+	// $('.js-sa-input').change(function(){
+	// 	$(this).parent('.sa-input__box').addClass('is-focus');
+	// });
+	$('.js-sa-input').change(function(){
+		if($(this).val() == ''){
+			$(this).parent('.sa-input__box').removeClass('is-focus');
+		}
+	});
+
+	//Authentification
+	$(".auth__item").not(":first").hide();
+	$(".auth__tab").click(function() {
+		$('.auth__tab').removeClass('is-active');
+		$(this).addClass('is-active');
+		$(".auth__item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("is-active");
+
+
+
+	//Mask
+	$('.phone-mask').mask("+7(999) 999-9999");
 
 	
 	// $(window).on("load",function(){
